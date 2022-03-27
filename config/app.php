e5123f36ab760a1fbbae0fe27ee9c167774a1612
Wanyeki <wanyekifs@gmail.com>
2022-03-27 23:36:49 +0300

    add api key

--- config/app.php
+++ config/app.php
@@ -124,7 +124,7 @@ return [
     'key' => env('APP_KEY'),
 
     'cipher' => 'AES-256-CBC',
-
+    'key' => env('APP_KEY', 'helloworld2345678909'),
     /*
     |--------------------------------------------------------------------------
     | Autoloaded Service Providers
